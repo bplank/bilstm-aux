e92f5e12cab9e3af78f6a1ea1175d6d460b48bca
Barbara Plank <b.plank@rug.nl>
2017-10-25 17:59:10 +0200

    testing the load_tagger/save_tagger function in simplebilty. OK!

--- src/run_simple.py
+++ src/run_simple.py
@@ -4,11 +4,13 @@
 ## python run_simply.py --dynet-seed 113
 
 from simplebilty import SimpleBiltyTagger
+from simplebilty import load_tagger, save_tagger
+
 import random
 ### Use --dynet-seed $SEED
 seed=113 # assume we pass this to script
-train_data = "/Users/bplank/corpora/pos/ud1.3/orgtok/goldpos/da-ud-dev.conllu"
-dev_data = "/Users/bplank/corpora/pos/ud1.3/orgtok/goldpos/da-ud-test.conllu"
+train_data = "data/da-ud-dev.conllu"
+dev_data = "data/da-ud-test.conllu"
 in_dim=64
 h_dim=100
 c_in_dim=100
@@ -17,7 +19,15 @@ iters=2
 trainer="sgd"
 tagger = SimpleBiltyTagger(in_dim, h_dim,c_in_dim,h_layers,embeds_file=None)
 train_X, train_Y = tagger.get_train_data(train_data)
+tagger.initialize_graph()
 tagger.fit(train_X, train_Y, iters, trainer,seed=seed)
 test_X, test_Y = tagger.get_data_as_indices(dev_data)
 correct, total = tagger.evaluate(test_X, test_Y)
+print(correct, total, correct/total)
+
+# test loading/saving
+save_tagger(tagger, "tmp")
+
+tagger2= load_tagger("tmp.model")
+correct, total = tagger2.evaluate(test_X, test_Y)
 print(correct, total, correct/total)
